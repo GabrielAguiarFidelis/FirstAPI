15106545da0aefd375a3adc13b9a9dc286758a79
Gabriel <gabriel.aguiar.fidelis13@gmail.com>
2026-03-23 11:40:14 -0300

    Up

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import usuariosRoutes from './src/routes/usuarios.js'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import usuariosRoutes from "./src/routes/usuarios.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json()) // ✅ TEM QUE VIR ANTES DAS ROTAS
+app.use(cors());
+app.use(express.json()); // ✅ TEM QUE VIR ANTES DAS ROTAS
 
-app.use('/usuarios', usuariosRoutes)
+app.use("/usuarios", usuariosRoutes);
 
-app.get('/', (req, res) => {
-  res.send('API Monetra rodando 🚀')
-})
+app.get("/", (req, res) => {
+  req.body; // ✅ para testar se o JSON está sendo lido
+  res.send("API Monetra rodando 🚀");
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
-})
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
